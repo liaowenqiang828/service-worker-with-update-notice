a36906065031abc312d5e5aa3bc09fbd17d7a431
liaowenqiang828 <18840965351@163.com>
2022-03-31 14:28:01 +0800

    [Wenqiang] add update handler

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,16 @@
-const cacheName = "firstVersion";
+const cacheName = "sixVersion";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheName).then((cache) => cache.addAll(["./GATX.png"]))
   );
+});
+
+self.addEventListener("message", function (event) {
+  console.log("event", event);
+  if (event.data.action === "skipWaiting") {
+    self.skipWaiting();
+  }
 });
 
 self.addEventListener("fetch", function (event) {
